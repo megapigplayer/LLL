4a2e55c4a984aea5241cc6234432f964f49ff4e5
megapigplayer <40871706+megapigplayer@users.noreply.github.com>
2018-07-06 01:26:46 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import time
+import os
 
 client = discord.Client ()
 Client = commands.Bot (command_prefix = "+")
