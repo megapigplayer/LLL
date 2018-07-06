0fc895cd6b8335bd5a40e41db6af9523838338c0
megapigplayer <40871706+megapigplayer@users.noreply.github.com>
2018-07-06 15:41:11 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -63,16 +63,16 @@ __**Staff Commands**__
         
         await bot.process_commands(message)
         
-@client.command(pass_context = True)
+@Client.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
      if ctx.message.author.server_permissions.mute_members or ctx.message.author.id == '194151340090327041':
         role = discord.utils.get(member.server.roles, name='Muted')
-        await client.add_roles(member, role)
+        await Client.add_roles(member, role)
         embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
-        await client.say(embed=embed)
+        await Client.say(embed=embed)
      else:
         embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
-        await client.say(embed=embed)
+        await Client.say(embed=embed)
         
         
 client.run(os.getenv('TOKEN'))
