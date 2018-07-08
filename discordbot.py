907b1b8d04aec9c0ba4c61426124b75cc8062164
megapigplayer <40871706+megapigplayer@users.noreply.github.com>
2018-07-08 23:36:20 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,10 @@ owner = ["Insert-Owner-ID"]
 async def on_ready():
     print("Bot Is ready!")
     await Client.change_status(game=discord.Game(name="+help | By MegaPig"))
-
+	
+@Client.command()
+async def cookie(ctx):
+    await ctx.message.add_reaction(emoji="🍪")
 
 @Client.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
