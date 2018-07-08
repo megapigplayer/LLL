ab24cf18a4d28be9dd36a590dc7e021dcbc684e5
megapigplayer <40871706+megapigplayer@users.noreply.github.com>
2018-07-08 22:07:13 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,7 @@ owner = ["Insert-Owner-ID"]
 @Client.event
 async def on_ready():
     print("Bot Is ready!")
-await Client.change_status(game=discord.Game(name="+help | By MegaPig"))
+    await Client.change_status(game=discord.Game(name="+help | By MegaPig"))
 
 
 @Client.command(pass_context = True)
