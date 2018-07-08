4cec2fa2ab455df844e7c2e5dd3ccfdf8d143422
megapigplayer <40871706+megapigplayer@users.noreply.github.com>
2018-07-08 19:04:57 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -131,5 +131,51 @@ async def removewarn(ctx, member: discord.Member):
 async def say(output):
     await Client.say(output)
 
+@Client.event
+async def on_message(message):
+    if message.content.startswith('+info'):
+        await Client.send_message(message.channel, """Hello! My name is MegaBot!
+I have 3 types of commands:
+**Information Commands**
+**General Commands**
+**Staff Commands**
+For see all the commands type: __+help__""")
+
+    if message.content.startswith('+creator'):
+        await Client.send_message(message.channel, """**My Creator Is MegaPig#1576!**
+__If you see bug in the bot say to him!__
+__And you can ask him for support!__""")
+        await Client.send_message(client.get_channel('log'), 'hello')
+        await Client.add_reaction('\U0001F44D')
+        
+    if message.content.startswith('+invite'):
+        await Client.send_message(message.channel, """https://discordapp.com/api/oauth2/authorize?client_id=459490391146627073&permissions=8&scope=bot
+__You Can Invite Me When Ever You Want!__
+You Can Help If You Say Bugs To My Creator- **MegaPig#1576**""")
+
+    if message.content.startswith('+help'):
+        embed = discord.Embed(title="Command List", description="""
+__**Information Commands**__
+**+info** - Small Information Of The Bot.
+**+invite** - The Bot Invite Link.
+**+creator** - The Bot Creator.
+__**General Commands**__
+**+avatar @tag** - The avatar of the member.
+**+servericon** - the picture of the server.
+**+servers** - how many servers the bot in.
+**+question (שאלת כן ולא)** - The Bot answear to you.
+__**Staff Commands**__
+**+warn** @TAG- warn member (move members permission)
+**+mute** @TAG- mute member (mute members permission)
+**+clear (2-100)** - clear messages (manage_messages permission)
+**+kick** @TAG - kick member (kick members permission)
+**+ban** @TAG - ban member (ban members permission)
+""", color=0xe88af4)
+        await Client.send_message(message.channel, embed=embed)
+
+    if message.content.startswith('תצעק'):
+        await Client.send_message(message.channel, "האהאהאההאהאהאהאהאהאהאהאאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאההאאהאההאהההאהאהאהאהאהאאהאהאהאהאהאהאאהאאאהאאאהאהאהאאהאההאהאאההאהאאהאהאהאהאאהאהאהאהאהאהאהאאהאהאהאהאהאאהאהאהאהאהאהאאהאהאהאהאאהאהאהאהאאהאהאהאהאהאהאהאהאההאההאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאהאההאהאהאאהאהאהאהאההאאהאהאהאהאהאהאהאהאהאהאאהאהאהאהאאההאאהאהאהאהאהאהאהאהאהאאאהאהאהאהאהאהאהאהאהאהה")
+
+                    
 
 Client.run(os.getenv('TOKEN'))
